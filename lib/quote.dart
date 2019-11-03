e6f477bb036ee13e045e3830a81f18991c7f099d
shiratensei <shiratensei92@gmail.com>
2019-11-03 09:50:29 +0800

    class quote added.

---
+++ lib/quote.dart
@@ -0,0 +1,12 @@
+
+class Quote {
+
+  String text;
+  String author;
+
+  Quote ({ this.text, this.author });
+
+}
+
+/// Below is an example of declaring such class.
+// Quote myQuote = Quote(author: "James Wild", text: "A quote.");
